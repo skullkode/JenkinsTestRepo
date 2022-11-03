792af00a64487560a70590db6a28afefd8add97c
skullkode <skullcode80@gmail.com>
2022-11-03 17:38:07 +0100

    first commit

---
+++ src/test/java/TestMain.java
@@ -0,0 +1,13 @@
+import org.example.Main;
+import org.junit.jupiter.api.Test;
+
+public class TestMain {
+
+    @Test
+    void testMain() {
+        System.out.println("Starting Test:");
+        Main.main(new String[] {"World"});
+    }
+
+
+}
